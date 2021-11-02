d8ee794b0da211888560c6f97e9357e1ecf44d62
jose <jabelcastellanosjoo@unm.edu>
2021-11-01 23:56:10 -0600

    Modularize symbols and presentation files.

---
+++ updatePresentation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from os import system as cmd
+
+cmd("rm -rf ./presentation.sty")
+cmd("touch ./presentation.sty")
+cmd("cat ./presentation-packages.sty >> ./presentation.sty")
+cmd("cat ./colors.sty >> ./presentation.sty")
+cmd("cat ./presentation-math-environment.sty >> ./presentation.sty")
+cmd("cat ./commandsAndDeclarations.sty >> ./presentation.sty")
